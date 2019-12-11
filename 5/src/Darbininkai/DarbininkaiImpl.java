f56ed02654599230670f45f0e0a35fa541343817
justass490 <55572439+justass490@users.noreply.github.com>
2019-12-12 00:24:00 +0200

    Add files via upload

---
+++ 5/src/Darbininkai/DarbininkaiImpl.java
@@ -0,0 +1,57 @@
+package Darbininkai;
+
+import java.util.Comparator;
+
+public class DarbininkaiImpl implements Comparator {
+
+        private String vardas;
+        private String detale;
+        private int detaliuKiekis;
+        private int detalesKaina;
+
+        public DarbininkaiImpl(String vardas, String detale, int detaliuKiekis, int detalesKaina){
+            this.vardas = vardas;
+            this.detale = detale;
+            this.detaliuKiekis = detaliuKiekis;
+            this.detalesKaina = detalesKaina;
+        }
+
+    public String getVardas(){
+            return vardas;
+        }
+        public void setVardas(String vardas){
+            this.vardas = vardas;
+        }
+        public String getDetale(){
+            return detale;
+        }
+        public void setDetale(String detale){
+            this.detale = detale;
+        }
+        public int getDetaliuKiekis(){
+            return detaliuKiekis;
+        }
+        public void setDetaliuKiekis(int detaliuKiekis){
+            this.detaliuKiekis = detaliuKiekis;
+        }
+        public int getDetalesKaina(){
+            return detalesKaina;
+        }
+        public void setDetalesKaina(int detalesKaina){
+            this.detalesKaina = detalesKaina;
+        }
+
+    @Override
+    public String toString() {
+        return "DarbininkaiImpl{" +
+                "vardas='" + vardas + '\'' +
+                ", detale='" + detale + '\'' +
+                ", detaliuKiekis=" + detaliuKiekis + '\'' +
+                ", detalesKaina=" + detalesKaina + "\n";
+    }
+
+    @Override
+    public int compare(Object o, Object t1) {
+        return 0;
+    }
+}
--- 5/src/Main.java
+++ 5/src/Main.java
@@ -1,25 +1,28 @@
+import Darbininkai.DarbininkaiImpl;
+
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Collections;
 
 public class Main{
-    private static void sort(ArrayList<Darbininkas> list) {
+    private static void sort(ArrayList<DarbininkaiImpl> list) {
     }
 
     //vardas, detale, detaliuKiekis, detalesKaina
     public static void main(String[] args) {
-        Darbininkas pirmas = new Darbininkas("Jonas", "a", 1001, 110);
-        Darbininkas antras = new Darbininkas("Justas", "b", 230, 15);
-        Darbininkas trecias = new Darbininkas("Kestas", "c", 1030, 25);
-        Darbininkas ketvirtas = new Darbininkas("Petras", "d", 170, 80);
-        Darbininkas penktas = new Darbininkas("Domas", "e", 150, 60);
-        Darbininkas sestas = new Darbininkas("Dominykas", "f", 200, 70);
-        Darbininkas septintas = new Darbininkas("Simas", "g", 1005,30);
-        Darbininkas astuntas = new Darbininkas("Tomas", "h", 300, 20);
-        Darbininkas devintas = new Darbininkas("Dovydas", "i", 150, 30);
-        Darbininkas desimtas = new Darbininkas("Lukas","j", 240, 70);
+        DarbininkaiImpl pirmas = new DarbininkaiImpl("Jonas", "a", 1001, 110);
+        DarbininkaiImpl antras = new DarbininkaiImpl("Justas", "b", 230, 15);
+        DarbininkaiImpl trecias = new DarbininkaiImpl("Kestas", "c", 1030, 25);
+        DarbininkaiImpl ketvirtas = new DarbininkaiImpl("Petras", "d", 170, 80);
+        DarbininkaiImpl penktas = new DarbininkaiImpl("Domas", "e", 150, 60);
+        DarbininkaiImpl sestas = new DarbininkaiImpl("Dominykas", "f", 200, 70);
+        DarbininkaiImpl septintas = new DarbininkaiImpl("Simas", "g", 1005,30);
+        DarbininkaiImpl astuntas = new DarbininkaiImpl("Tomas", "h", 300, 20);
+        DarbininkaiImpl devintas = new DarbininkaiImpl("Dovydas", "i", 150, 30);
+        DarbininkaiImpl desimtas = new DarbininkaiImpl("Lukas","j", 240, 70);
 
-        ArrayList<Darbininkas> list = new ArrayList<>();
+        ArrayList<DarbininkaiImpl> list = new ArrayList<>();
         list.add(pirmas);
         list.add(antras);
         list.add(trecias);
@@ -33,42 +36,42 @@ public class Main{
         System.out.println("---------------------------------------------");
         System.out.println("|Sarasas|");
         System.out.println(" ");
-        for (Darbininkas i : list){
+        for (DarbininkaiImpl i : list){
             System.out.println(i);
         }
         System.out.println("---------------------------------------------");
         System.out.println("|Pagamintu detaliu dienos suvestine|");
         System.out.println(" ");
-        for (Darbininkas i : list){
+        for (DarbininkaiImpl i : list){
             double suma = i.getDetalesKaina() * i.getDetaliuKiekis();
             System.out.println("<detale: " + i.getDetale() + " > <kiekis: " + i.getDetaliuKiekis() + " > <suma: " + suma);
         }
         System.out.println("---------------------------------------------");
         System.out.println("|Suvestine pagal detaliu skaiciu|");
         System.out.println(" ");
-        list.sort(Comparator.comparing(Darbininkas::getDetaliuKiekis, Collections.reverseOrder()).thenComparing(Darbininkas::getDetale
-	System.out.println(list);
-        }
+        list.sort(Comparator.comparing(DarbininkaiImpl::getDetaliuKiekis, Collections.reverseOrder()).thenComparing(DarbininkaiImpl::getDetale));
+                System.out.println(list);
+
         System.out.println("---------------------------------------------");
         System.out.println("|Normos ivykdymas|");
         System.out.println(" ");
-        Iterator<Darbininkas> i = list.iterator();
+    Iterator<DarbininkaiImpl> i = list.iterator();
         while (i.hasNext()) {
-            Darbininkas zmogus = i.next();
-            if (zmogus.getDetaliuKiekis() < 1000) {
-                System.out.println("darbininkas: " + zmogus.getVardas() + ", detale " + zmogus.getDetale() + ", kiekis: "
-                        + zmogus.getDetaliuKiekis() + ", kaina: " + zmogus.getDetalesKaina() + " - norma neivykdita");
-            } else{
-                System.out.println("darbininkas: " + zmogus.getVardas() + ", detale " + zmogus.getDetale() + ", kiekis: "
-                        + zmogus.getDetaliuKiekis() + ", kaina: " + zmogus.getDetalesKaina() + " - norma ivykdyta");
-            }
-            if (zmogus.getDetalesKaina() > 100)
-                i.remove();
+            DarbininkaiImpl zmogus = i.next();
+        if (zmogus.getDetaliuKiekis() < 1000) {
+            System.out.println("darbininkas: " + zmogus.getVardas() + ", detale " + zmogus.getDetale() + ", kiekis: "
+                    + zmogus.getDetaliuKiekis() + ", kaina: " + zmogus.getDetalesKaina() + " - norma neivykdita");
+        } else{
+            System.out.println("darbininkas: " + zmogus.getVardas() + ", detale " + zmogus.getDetale() + ", kiekis: "
+                    + zmogus.getDetaliuKiekis() + ", kaina: " + zmogus.getDetalesKaina() + " - norma ivykdyta");
         }
+        if (zmogus.getDetalesKaina() > 100)
+            i.remove();
+    }
         System.out.println("---------------------------------------------");
         System.out.println("|Detales, zemesnes nei 100e|");
         System.out.println(" ");
         System.out.println(list);
         System.out.println("---------------------------------------------");
-    }
+}
 }
